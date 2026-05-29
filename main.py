1357efc3ab8ba515f5e2fa238b6796af57de059d
AKHIL REDDY KOLANU <133190450+akhilakki12@users.noreply.github.com>
2026-05-29 17:01:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.data_loader import load_all_documents
 from src.vectorstore import FaissVectorStore
 from src.search import RAGSearch
-# Example usage
 if __name__ == "__main__":
     
     #docs = load_all_documents("Data")
